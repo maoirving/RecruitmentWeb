63db7abe2ebfe0ae17fbde74310d7f797e82d888
mao <1559304351@qq.com>
2021-08-23 21:11:04 +0800

    config file fix

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//   css: {
-//     // requireModuleExtension: false,
-//     loaderOptions: {
-//       sass: {
-//         prependData: `@import "@/assets/sass/app.scss";`
-//       }
-//     }
-//   }
-// }
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('import-glob')
+      .test(/\.scss/)
+      .use('import-glob-loader2')
+      .loader('import-glob-loader2')
+      .end()
+  }
+}
