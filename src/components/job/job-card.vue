4ee2725e9afd6561b8c14fe862b78f3c29988885
junjie <junjie@kooboo.net>
2021-09-01 12:45:15 +0800

    fix: style for job-card

--- src/components/job/job-card.vue
+++ src/components/job/job-card.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="job-card" @click="toJobDetail">
-    <div class="job-card-content">
-      <div class="title-holder">
-        <h4 class="font-normal text-base text-green-600">
-          {{ job.name }}
-        </h4>
-        <div class="content-tag">{{ job.salary }}</div>
-      </div>
-
-      <p v-if="!isSimpleType" class="content-text">
-        <span class="text-xs">{{ description }}</span>
+    <div class="job-card-header">
+      <h4 class="text-base truncate header-title">
+        {{ job.name }}
+      </h4>
+      <div class="header-tag">{{ job.salary }}</div>
+    </div>
+    <div class="job-card-body">
+      <p v-if="!isSimpleType" class="body-content">
+        <span class="text-xs">{{ job.city }}</span>
+        <i class="v-line"></i>
+        <span class="text-xs">{{ job.experience }}</span>
+        <i class="v-line"></i>
+        <span class="text-xs">{{ job.education }}</span>
       </p>
     </div>
     <div class="job-card-footer">
@@ -17,16 +20,21 @@
         <el-col :span="4">
           <base-aspect data-ratio="1:1" isCircle with-shadow>
             <div class="image-holder" @click.stop="toCompanyDetail">
-              <img :src="job.imageUrl" alt="" />
+              <img :src="job.company.imageUrl" />
             </div>
           </base-aspect>
         </el-col>
         <el-col class="flex items-center" :span="20">
-          <div class="text-holder" @click.stop="toCompanyDetail">
-            <span class="footer-info">
+          <div class="truncate text-holder" @click.stop="toCompanyDetail">
+            <span class="truncate text-gray-600 footer-text main-text">
               {{ job.company.name }}
             </span>
-            <span v-if="!isSimpleType" class="footer-info">
+            <i class="v-line"></i>
+            <span class="truncate footer-text short-text">
+              {{ job.company.category }}
+            </span>
+            <i class="v-line"></i>
+            <span v-if="!isSimpleType" class="truncate footer-text short-text">
               {{ job.company.financingStage }}
             </span>
           </div>
@@ -55,12 +63,6 @@ export default {
     }
   },
 
-  computed: {
-    description() {
-      return `${this.job.city} ｜ ${this.job.experience} ｜ ${this.job.education}`
-    }
-  },
-
   methods: {
     toJobDetail() {
       this.$router.push(`/job/detail?jobId=${this.job.id}`)
@@ -75,25 +77,54 @@ export default {
 
 <style lang="scss" scoped>
 .job-card {
-  padding: 18px 12px;
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 1px 1px 5px rgba($color: #000000, $alpha: 0.3);
+  padding: $gap-sm $gap;
+  background-color: $c-white;
+  border-radius: $gap-xs;
   cursor: pointer;
-  transition: all 0.3s;
+  border: $border-gray;
+  transition: $transition;
   &:hover {
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
     transform: translateY(-2px);
   }
-  &-content {
-    .title-holder {
-      display: flex;
+  &-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 8px;
+    .header-title {
+      max-width: 80%;
+      font-weight: 400;
+      color: $c-green-600;
+      margin-bottom: 0;
+    }
+    .header-tag {
+      color: $c-red-400;
+    }
+  }
+  &-body {
+    margin-bottom: 8px;
 
-      justify-content: space-between;
+    .body-content {
+      margin-bottom: 0;
+    }
+  }
+  &-footer {
+    .image-holder {
+      height: 100%;
+    }
+    .text-holder {
+      display: flex;
       align-items: center;
-      .content-title {
-      }
-      .content-tag {
-        color: rgb(240, 54, 54);
+      .footer-text {
+        display: inline-block;
+        font-size: 13px;
+        &.main-text {
+          max-width: 90px;
+        }
+        &.short-text {
+          max-width: 50px;
+        }
       }
     }
   }
